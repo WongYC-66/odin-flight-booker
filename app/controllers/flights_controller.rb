bd6aacfec3dd0bf2ad244a33e422b0f1a35a5680
WongYC-66 <yengchien@hotmail.com>
2024-11-04 17:33:30 +0800

    add : pick a flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,12 +1,20 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
     @airports = Airport.all.map { |a| [ a.name, a.name ] }
+    @flight_dates = Flight.all.map { |f| [ f.start.strftime("%Y/%m/%d"), f.start.strftime("%Y%m%d") ] }.to_set
     @num_tickets = (1..4).map { |n| [ n, n ] }
-    @flight_dates = Flight.all.map { |f| [ f.start.strftime("%Y/%m/%d"), f.start.strftime("%Y%m%d") ] }
 
     puts ""
-    puts params
+    if params[:departure_code] && params[:arrival_code] && params[:date]
+      puts params if params
+      departure_airport = Airport.find_by(name: params[:departure_code])
+      arrival_airport = Airport.find_by(name: params[:arrival_code])
+      @flights = Flight
+        .where(departure_airport_id: departure_airport.id)
+        .where(arrival_airport_id: arrival_airport.id)
+        .includes(:departure_airport, :arrival_airport)
+      @ticket_count = params[:num_tickets]
+    end
     puts ""
   end
 end
